fc39cceb8c9aae388925160e1c0873320b8d3dea
brguo55 <brguo55@gmail.com>
2024-03-30 08:20:22 -0400

    removing roster variable

--- roster.py
+++ roster.py
@@ -31,3 +31,12 @@ data = pd. DataFrame (player)
 print(data)
 
 # 5
+import pandas as pd
+
+player = {"Last Name":["Bacot", "Davis", "Cadeau"],
+          "First Name": ["Armando", "RJ", "Elliot"],
+          "height": [83, 72, 731,
+          "weight": [240,180,180]
+           }
+data = pd.DataFrame (player)
+print(data)
