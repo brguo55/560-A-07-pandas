733218feac7e1434e6f7f0309969cd47dbc17d53
brguo55 <brguo55@gmail.com>
2024-03-30 08:17:22 -0400

    expanded dictionary

--- roster.py
+++ roster.py
@@ -1,5 +1,33 @@
 # https://goheels.com/sports/mens-basketball/roster
 
+# 2a
+import pandas as pd
+roster = ['Bacot', 'Davis', 'Ryan']
+data = pd.DataFrame(roster)
+print(data)
+
+
+'''
 roster = ['Bacot', 'Davis', 'Ryan']
 for player in roster:
     print(player)
+'''
+
+# 3a
+import pandas as pd
+roster = ["Bacot", "Davis", "Cadeau"]
+player = {"Last Name": roster}
+data = pd. DataFrame (player)
+print
+
+# 4
+import pandas as pd
+roster = ["Bacot", "Davis", "Cadeau"]
+Player = {"Last Name": roster,
+"First Name": ["Armando", "RJ", "Elliot"],
+"height": [83, 72, 731,
+"weight": [240,180, 1801}
+data = pd. DataFrame (player)
+print(data)
+
+# 5
