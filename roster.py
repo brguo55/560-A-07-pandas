7426794db3b8878532348017f5780df382ccbb8c
brguo55 <brguo55@gmail.com>
2024-03-30 19:33:16 -0400

    roster list for asgn7

--- roster.py
+++ roster.py
@@ -2,18 +2,31 @@
 
 import pandas as pd
 
+def feet_to_meters(feet, inches):
+    meters = (feet * 12 + inches) * 0.0254
+    return round(meters, 4)
+
+# Assuming that the 'height' list is meant to be feet and inches,
+# e.g., 6.9 should be interpreted as 6 feet 9 inches, not 6.9 feet.
+# If 6.11 is 6 feet 11 inches, and 6.10 is 6 feet 10 inches.
+heights_in_feet = [6.9, 6.1, 6.5, 6.0, 6.11, 6.3, 6.5, 6.10, 6.1, 6.4, 6.8, 6.7, 6.7]
+heights_in_meters = [feet_to_meters(int(h), (h*10 % 10)*12 if '.10' in str(h) else (h % 1)*10) for h in heights_in_feet]
+
 player = {
-        "Last Name": ['High', 'Cadeau', 'Ryan', 'Davis', 'Bacot', 'Trimble', 'Wojcik', 'Washington', 'Lebo', 'Landry,', 'Okonkwo', 'Farris', 'Ingram'],
-        "First Name": ['Zayden', 'Elliot', 'Cormac', 'RJ', 'Armando', 'Seth', 'Paxson', 'Jalen', 'Creighton', 'Rob', 'James', 'Duwe', 'Harrison'],
-        "height": [83, 72, 73],
-        "weight": [240,180,180]
-         }   
+    "Last Name": ['High', 'Cadeau', 'Ryan', 'Davis', 'Bacot', 'Trimble', 'Wojcik', 'Washington', 'Lebo', 'Landry', 'Okonkwo', 'Farris', 'Ingram'],
+    "First Name": ['Zayden', 'Elliot', 'Cormac', 'RJ', 'Armando', 'Seth', 'Paxson', 'Jalen', 'Creighton', 'Rob', 'James', 'Duwe', 'Harrison'],
+    "height": heights_in_meters,
+    "weight": [225, 180, 195, 180, 240, 195, 195, 230, 180, 190, 240, 210, 225]
+}
 
-data = pd. DataFrame (player)
+data = pd.DataFrame(player)
 
-data['bmi'] = (data['weight']/2.205)/((data['height']/39.37)**2)
+# Calculate BMI
+data['bmi'] = (data['weight'] / 2.205) / (data['height'] ** 2)
 data['bmi'] = data['bmi'].round(2)
 
-print (data)
+print(data)
 
-data.to_csv('bmi.csv')
+# Save to CSV
+data.to_csv('bmi.csv', index=False)
+
