7de43c94e1d4a31f725a04f3d26ffd1a7d9f2a34
brguo55 <brguo55@gmail.com>
2024-03-30 19:14:08 -0400

    need a new version

--- roster.py
+++ roster.py
@@ -1,58 +1,16 @@
-# https://goheels.com/sports/mens-basketball/roster
+# 7 Official 2023-24 UNC Basketball Roster
 
-# 2a
-import pandas as pd
-roster = ['Bacot', 'Davis', 'Ryan']
-data = pd.DataFrame(roster)
-print(data)
-
-'''
-roster = ['Bacot', 'Davis', 'Ryan']
-for player in roster:
-    print(player)
-'''
-
-# 3a
-import pandas as pd
-roster = ["Bacot", "Davis", "Cadeau"]
-player = {"Last Name": roster}
-data = pd. DataFrame (player)
-print
-
-# 4
-import pandas as pd
-roster = ["Bacot", "Davis", "Cadeau"]
-player = {"Last Name":["Bacot", "Davis", "Cadeau"],
-          "First Name": ["Armando", "RJ", "Elliot"],
-          "height": [83, 72, 731],
-          "weight": [240,180,180]
-           }
-data = pd. DataFrame (player)
-print(data)
-
-# 5
 import pandas as pd
 
-player = {"Last Name":["Bacot", "Davis", "Cadeau"],
-          "First Name": ["Armando", "RJ", "Elliot"],
-          "height": [83, 72, 731],
-          "weight": [240,180,180]
-           }
-data = pd.DataFrame (player)
-print(data)
-
-# 6
-import pandas as pd
-
-player = {"Last Name":["Bacot", "Davis", "Cadeau"],
-          "First Name": ["Armando", "RJ", "Elliot"],
-          "height": [83, 72, 731],
-          "weight": [240,180,180]
-           }
+player = {
+        "Last Name": ['High', 'Cadeau', 'Ryan', 'Davis', 'Bacot', 'Trimble', 'Wojcik', 'Washington', 'Lebo', 'Landry,', 'Okonkwo', 'Farris', 'Ingram'],
+        "First Name": ['Zayden', 'Elliot', 'Cormac', 'RJ', 'Armando', 'Seth', 'Paxson', 'Jalen', 'Creighton', 'Rob', 'James', 'Duwe', 'Harrison'],
+        "height": [83, 72, 73],
+        "weight": [240,180,180]
+         }   
 
 data = pd. DataFrame (player)
 
-# bmi = weight in kg/ height in meters squared
 data['bmi'] = (data['weight']/2.205)/((data['height']/39.37)**2)
 data['bmi'] = data['bmi'].round(2)
 
---
+++ roster_example.py
@@ -0,0 +1,61 @@
+# https://goheels.com/sports/mens-basketball/roster
+
+# 2a
+import pandas as pd
+roster = ['Bacot', 'Davis', 'Ryan']
+data = pd.DataFrame(roster)
+print(data)
+
+'''
+roster = ['Bacot', 'Davis', 'Ryan']
+for player in roster:
+    print(player)
+'''
+
+# 3a
+import pandas as pd
+roster = ["Bacot", "Davis", 'Ryan']
+player = {"Last Name": roster}
+data = pd. DataFrame (player)
+print
+
+# 4
+import pandas as pd
+roster = ["Bacot", "Davis", 'Ryan']
+player = {"Last Name":["Bacot", "Davis", 'Ryan'],
+          "First Name": ["Armando", "RJ", "Cormac"],
+          "height": [83, 72, 73],
+          "weight": [240,180,180]
+           }
+data = pd. DataFrame (player)
+print(data)
+
+# 5
+import pandas as pd
+
+player = {"Last Name":["Bacot", "Davis", "Ryan"],
+          "First Name": ["Armando", "RJ", "Cormac"],
+          "height": [83, 72, 73],
+          "weight": [240,180,180]
+           }
+data = pd.DataFrame (player)
+print(data)
+
+# 6
+import pandas as pd
+
+player = {"Last Name":["Bacot", "Davis", 'Ryan'],
+          "First Name": ["Armando", "RJ", "Cormac"],
+          "height": [83, 72, 73],
+          "weight": [240,180,180]
+           }
+
+data = pd. DataFrame (player)
+
+# bmi = weight in kg/ height in meters squared
+data['bmi_example'] = (data['weight']/2.205)/((data['height']/39.37)**2)
+data['bmi_example'] = data['bmi'].round(2)
+
+print (data)
+
+data.to_csv('bmi_example.csv')
