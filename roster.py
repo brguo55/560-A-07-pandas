35c39d4c6c24090cec20ff27c6f73490a6ae2077
brguo55 <brguo55@gmail.com>
2024-03-29 20:59:56 -0400

    roster as loop

--- roster.py
+++ roster.py
@@ -1,4 +1,5 @@
-https://goheels.com/sports/mens-basketball/roster
+# https://goheels.com/sports/mens-basketball/roster
 
 roster = ['Bacot', 'Davis', 'Ryan']
-print(roster)
+for player in roster:
+    print(player)
